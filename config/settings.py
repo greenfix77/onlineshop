d7274813bb9d65447c01a0f877c5e97e9776d237
greenfix77 <glxmobile59@gmail.com>
2025-01-27 18:02:00 +0330

    real email sending

--- config/settings.py
+++ config/settings.py
@@ -62,6 +62,12 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 578
+# EMAIL_HOST_USER =''
+# EMAIL_HOST_PASSWORD =''
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
