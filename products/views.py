a5a2e4ee823f91695bdf8894558ecbe9d7a36b0b
greenfix77 <glxmobile59@gmail.com>
2025-01-29 08:50:23 +0330

    detail view and absolute url

--- products/views.py
+++ products/views.py
@@ -9,5 +9,11 @@ class ProductListView(generic.ListView):
     template_name = 'products/product_list.html'
     context_object_name = 'products'
 
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+
+
 
 
